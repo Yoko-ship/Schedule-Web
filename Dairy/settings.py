1925fb6f7e6eb4cdb0a82d33bba847e522208ffd
User <yuko.brandon13@gmail.com>
2025-01-28 20:44:01 +0500

    debug

--- Dairy/settings.py
+++ Dairy/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["secret_key"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['schedule-web.onrender.com', '127.0.0.1', 'localhost']
 
